5cd5598a115dcec0d227ae67748b41d57dd04d26
devanshtivari <devansh80771@gmail.com>
2021-10-30 14:57:51 +0530

    Preorder of a binary tree

---
+++ pre-order.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+//Preorder : +LR
+
+class Node
+{
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val)
+    {
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+void preorder(Node* &root)
+{
+    if(root != NULL){
+        cout<<root->data<<" ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    /*
+                        1
+                      /   \
+                    2      3
+                  /   \   /  \
+                 4     5 6    7
+    */
+    cout<<"Pre-order of binary tree is : ";
+    preorder(root);
+}
